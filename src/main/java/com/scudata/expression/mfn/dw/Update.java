75b492845741a48315fcc2356c6c2b0c84dc033e
liwei <65002112@qq.com>
2022-01-26 14:47:58 +0800

    add option @w of update function.

--- src/main/java/com/scudata/expression/mfn/dw/Update.java
+++ src/main/java/com/scudata/expression/mfn/dw/Update.java
@@ -6,6 +6,8 @@ import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
 import com.scudata.dm.Context;
 import com.scudata.dm.Sequence;
+import com.scudata.dm.cursor.ICursor;
+import com.scudata.dw.ColumnTableMetaData;
 import com.scudata.expression.TableMetaDataFunction;
 import com.scudata.resources.EngineMessage;
 
@@ -22,7 +24,10 @@ public class Update extends TableMetaDataFunction {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("update" + mm.getMessage("function.missingParam"));
 		}
+		
 		boolean hasN = opt != null && opt.indexOf('n') != -1;
+		boolean hasW = opt != null && opt.indexOf('w') != -1;
+		
 		Object obj = param.getLeafExpression().calculate(ctx);
 		if (obj instanceof Sequence) {
 			try {
@@ -31,6 +36,46 @@ public class Update extends TableMetaDataFunction {
 					return table;
 				}
 				return table.update((Sequence)obj, opt);
+			} catch (IOException e) {
+				throw new RQException(e);
+			}
+		} else if (hasW && obj instanceof ICursor && table instanceof ColumnTableMetaData) {
+			try {
+				ICursor cs = (ICursor)obj;
+				/**
+				 * 用游标更新时不能增加记录,
+				 * 所以不能出现@i
+				 */
+				if (opt != null && opt.indexOf('i') != -1) {
+					opt = opt.replace("i", "");
+				}
+				
+				/**
+				 * 增加@u
+				 */
+				if (opt == null) {
+					opt = "u";
+				} else {
+					opt += "u";
+				}
+				
+				if (!hasN) {
+					Sequence seq = cs.fetch(ICursor.FETCHCOUNT);
+					while(seq != null && seq.length() > 0) {
+						table.update(seq, opt);
+						seq = cs.fetch(ICursor.FETCHCOUNT);
+					}
+					return table;
+				} else {
+					Sequence result = new Sequence();
+					Sequence seq = cs.fetch(ICursor.FETCHCOUNT);
+					while(seq != null && seq.length() > 0) {
+						Sequence temp = table.update(seq, opt);
+						result.addAll(temp);
+						seq = cs.fetch(ICursor.FETCHCOUNT);
+					}
+					return result;
+				}
 			} catch (IOException e) {
 				throw new RQException(e);
 			}
