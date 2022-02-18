3cf486b006ccda297f6fe93c2e6617c04ecac169
wunanraq <wunan@raqsoft.com>
2022-02-18 10:40:59 +0800

    Add jdbc consts

--- src/main/java/com/esproc/jdbc/JDBCConsts.java
+++ src/main/java/com/esproc/jdbc/JDBCConsts.java
@@ -54,6 +54,19 @@ public class JDBCConsts {
 	public static final String TABLE_NAME = "TABLE_NAME";
 	public static final String COLUMN_NAME = "COLUMN_NAME";
 	public static final String DATA_TYPE = "DATA_TYPE";
+	
+	public static final String PKTABLE_CAT = "PKTABLE_CAT";
+	public static final String PKTABLE_SCHEM = "PKTABLE_SCHEM";
+	public static final String PKTABLE_NAME = "PKTABLE_NAME";
+	public static final String PKCOLUMN_NAME = "PKCOLUMN_NAME";
+
+	public static final String FKTABLE_CAT = "FKTABLE_CAT";
+	public static final String FKTABLE_SCHEM = "FKTABLE_SCHEM";
+	public static final String FKTABLE_NAME = "FKTABLE_NAME";
+	public static final String FKCOLUMN_NAME = "FKCOLUMN_NAME";
+
+	public static final String FK_NAME = "FK_NAME";
+	public static final String PK_NAME = "PK_NAME";
 
 	/** JDBC支持的数据文件类型 **/
 	public static final String DATA_FILE_EXTS = AppConsts.FILE_BTX + "," + AppConsts.FILE_TXT + "," + AppConsts.FILE_CSV
