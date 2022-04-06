3cabb766da1c4efe3b462dd2b0a88dc91cac3fc5
wunanraq <wunan@raqsoft.com>
2022-04-06 09:46:31 +0800

    Modify the parameters of the T function.

--- src/main/java/com/scudata/expression/fn/T.java
+++ src/main/java/com/scudata/expression/fn/T.java
@@ -14,7 +14,9 @@ import com.scudata.expression.IParam;
 import com.scudata.resources.EngineMessage;
 
 /**
- * T(fn,A;Fi,…;s) 根据文件扩展名读出文件返回成序表，txt/csv/xls/xlsx/btx/ctx
+ * 旧：T(fn,A;Fi,…;s)
+ * 新：T(fn:A,Fi,…;s)
+ * 根据文件扩展名读出文件返回成序表，txt/csv/xls/xlsx/btx/ctx
  * 文本时s是分隔符，xls时s为sheet名 有A时排列/游标A将写入，用替换写
  * 
  * @b 没有标题，缺省认为有标题
@@ -32,49 +34,107 @@ public class T extends Function {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("T" + mm.getMessage("function.missingParam"));
 		}
+		boolean isOldParam = false;
 		IParam fnParam, fieldParam = null, sParam = null;
+		String fields = null;
 		if (param.getType() == IParam.Semicolon) {
 			int subParamCount = param.getSubSize();
-			if (subParamCount != 2 && subParamCount != 3) {
+			if (subParamCount == 3) { // T(fn,A;Fi,…;s)
+				isOldParam = true;
+				fnParam = param.getSub(0);
+				fieldParam = param.getSub(1);
+				sParam = param.getSub(2);
+			} else if (subParamCount == 2) {
+				// T(fn,A;Fi,…)或者T(fn:A,Fi,…;s)
+				fnParam = param.getSub(0);
+				IParam sub2 = param.getSub(1);
+				if (sub2 != null && sub2.getType() == IParam.Comma) {
+					// T(fn,A;Fi,…)
+					isOldParam = true;
+					fieldParam = param.getSub(1);
+				} else {
+					// T(fn:A,Fi,…;s)
+					sParam = param.getSub(1);
+				}
+			} else {
 				MessageManager mm = EngineMessage.get();
 				throw new RQException("T"
 						+ mm.getMessage("function.invalidParam"));
 			}
-			fnParam = param.getSub(0);
-			fieldParam = param.getSub(1);
-			if (subParamCount > 2) {
-				sParam = param.getSub(2);
-			}
 		} else {
-			fnParam = param; // 只有fn,A
+			fnParam = param; // fn:A,Fi,...或者fn,A
 		}
 		if (fnParam == null) {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("T" + mm.getMessage("function.missingParam"));
 		}
 		IParam aParam = null;
-		String A = null;
 		if (fnParam.getType() == IParam.Comma) {
-			if (fnParam.getSubSize() != 2) {
+			if (fnParam.getSubSize() == 2) { // fn:A,Fi,...或者fn,A
+				IParam fnSub1 = fnParam.getSub(0);
+				IParam fnSub2 = fnParam.getSub(1);
+				if (fnSub1 == null) {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("T"
+							+ mm.getMessage("function.missingParam"));
+				}
+				if (fnSub1.getType() == IParam.Colon) { // fn:A
+					isOldParam = false;
+				} else {
+					if (fnSub2 != null) {
+						if (fnSub2.isLeaf()) {
+							try {
+								Object obj = fnSub2.getLeafExpression()
+										.calculate(ctx);
+								if (obj != null) {
+									if (obj instanceof ICursor
+											|| obj instanceof Sequence) {
+										isOldParam = true; // fn,A
+									}
+								}
+							} catch (Exception ex) {
+							}
+						}
+					}
+				}
+				if (isOldParam) {
+					aParam = fnParam.getSub(1);
+					fnParam = fnParam.getSub(0);
+				} else {
+					fieldParam = fnParam.getSub(1);
+					fnParam = fnParam.getSub(0);
+				}
+			} else if (fnParam.getSubSize() > 2) { // fn:A,Fi,...
+				isOldParam = false;
+				int count = fnParam.getSubSize();
+				StringBuffer buf = new StringBuffer();
+				for (int i = 1; i < count; ++i) {
+					IParam sub = fnParam.getSub(i);
+					if (sub == null || !sub.isLeaf()) {
+						MessageManager mm = EngineMessage.get();
+						throw new RQException("T"
+								+ mm.getMessage("function.invalidParam"));
+					}
+					if (i > 1)
+						buf.append(",");
+					buf.append(sub.getLeafExpression().toString());
+				}
+				fields = buf.toString();
+				fnParam = fnParam.getSub(0);
+			} else {
 				MessageManager mm = EngineMessage.get();
 				throw new RQException("T"
 						+ mm.getMessage("function.invalidParam"));
 			}
-			aParam = fnParam.getSub(1);
-			if (aParam != null) {
-				if (!aParam.isLeaf()) {
-					MessageManager mm = EngineMessage.get();
-					throw new RQException("T"
-							+ mm.getMessage("function.invalidParam"));
-				}
-				A = aParam.getLeafExpression().toString();
-			}
-			fnParam = fnParam.getSub(0);
 		}
 		if (fnParam == null) {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("file"
 					+ mm.getMessage("function.missingParam"));
+		}
+		if (fnParam.getType() == IParam.Colon) { // fn:A
+			aParam = fnParam.getSub(1);
+			fnParam = fnParam.getSub(0);
 		}
 		if (!fnParam.isLeaf()) {
 			MessageManager mm = EngineMessage.get();
@@ -95,8 +155,9 @@ public class T extends Function {
 		}
 		String fileName = (String) fn; // 计算后的文件名
 
-		String fields = null;
-		if (fieldParam != null) {
+		if (StringUtils.isValidString(fields)) {
+			// T(fn:A,Fi,…;s)
+		} else if (fieldParam != null) {
 			if (fieldParam.isLeaf()) {
 				// 这里不取getIdentifierName，保留单引号
 				fields = fieldParam.getLeafExpression().toString(); // getIdentifierName
@@ -120,6 +181,11 @@ public class T extends Function {
 		String s = null;
 		if (sParam != null) {
 			s = sParam.getLeafExpression().toString();
+		}
+
+		String A = null;
+		if (aParam != null) {
+			A = aParam.getLeafExpression().toString();
 		}
 
 		boolean hasTitle = opt == null || opt.indexOf("b") == -1;
