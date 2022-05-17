d077118afe6178a21217a94fa5cd867e787638aa
hdw <460778537@qq.com>
2022-05-17 17:04:14 +0800

    change SpringDBSessionFactory return type

--- src/main/java/com/scudata/common/SpringDBSessionFactory.java
+++ src/main/java/com/scudata/common/SpringDBSessionFactory.java
@@ -31,14 +31,22 @@ public class SpringDBSessionFactory implements ISessionFactory{
 	 * @param springDataSourceId bean name, equal to raqsoftConfig.xml datasource id
 	 * @param type DBType
 	 */
-	public static void create(String springDataSourceId, int type) {
+	public static ISessionFactory create(String springDataSourceId, int type) {
 		BeanFactoryDataSourceLookup lookup = new BeanFactoryDataSourceLookup();
 		BeanFactoryDataSourceLookup curr = (BeanFactoryDataSourceLookup) lookup;
+		try{
+			assert ImportSpringBeanFactory.beanFactory != null;
+		}catch(Exception e) {
+			Logger.debug("please scan com.scudata.common.ImportSpringBeanFactory");
+			return null;
+		}
 		BeanFactory beanFactory = ImportSpringBeanFactory.beanFactory;
 		curr.setBeanFactory(beanFactory);
+		//when not exists, spring will print FAILED/REQUIRED log
 		DataSource dataSource = lookup.getDataSource(springDataSourceId);
 		ISessionFactory sf = new SpringDBSessionFactory(dataSource, type);
 		Env.setDBSessionFactory( springDataSourceId, sf );
+		return sf;
 	}
 	
 }
