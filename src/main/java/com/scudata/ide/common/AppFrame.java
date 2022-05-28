f612170e4d5298b212e603591da08490ad1f9ca4
wunanraq <wunan@raqsoft.com>
2022-05-28 11:45:26 +0800

    Modify release date

--- src/main/java/com/scudata/ide/common/AppFrame.java
+++ src/main/java/com/scudata/ide/common/AppFrame.java
@@ -211,7 +211,7 @@ public abstract class AppFrame extends JFrame implements IAppFrame {
 	 * @return
 	 */
 	public String getReleaseDate() {
-		return "2022-05-27";
+		return "2022-04-02";
 	}
 
 	/**
