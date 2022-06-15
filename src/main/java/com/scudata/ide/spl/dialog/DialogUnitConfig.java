b5315f2afa5b7dff37fe607eb7019d1daada2810
XiaoQiongBao <xiaoqiong@raqsoft.com>
2022-06-15 14:51:57 +0800

    Modify resources of DialogUnitConfig

--- src/main/java/com/scudata/ide/spl/dialog/DialogUnitConfig.java
+++ src/main/java/com/scudata/ide/spl/dialog/DialogUnitConfig.java
@@ -6,7 +6,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileOutputStream;
