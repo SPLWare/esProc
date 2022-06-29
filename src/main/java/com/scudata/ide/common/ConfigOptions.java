d7afc58404e485231ec2a8a0123e3d31b0565114
wunanraq <wunan@raqsoft.com>
2022-06-29 17:36:58 +0800

    Modify auto save

--- src/main/java/com/scudata/ide/common/ConfigOptions.java
+++ src/main/java/com/scudata/ide/common/ConfigOptions.java
@@ -64,7 +64,7 @@ public class ConfigOptions {
 	/** 自动保存时间间隔（分钟） */
 	public static Integer iAutoSaveMinutes = new Integer(10);
 	/** 新建文件备份到目录 */
-	public static String sBackupDirectory = GM.getAbsolutePath(GC.PATH_BACKUP);
+	public static String sBackupDirectory = GC.PATH_BACKUP;
 	/** 自动打开的文件数量 */
 	public static String sAutoOpenFileNames = null;
 
