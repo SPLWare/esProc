59f0299743add511e87dd8db92cd250fdb5dd197
liwei <65002112@qq.com>
2022-07-11 17:48:32 +0800

    fix bug of import

--- src/main/java/com/scudata/expression/mfn/dw/Import.java
+++ src/main/java/com/scudata/expression/mfn/dw/Import.java
@@ -16,29 +16,6 @@ import com.scudata.expression.mfn.sequence.Top;
 public class Import extends TableMetaDataFunction {
 	public Object calculate(Context ctx) {
 		ICursor cursor = CreateCursor.createCursor(table, param, option, ctx);
-		Expression filter = null;
-		IParam expParam = param.getSub(1);
-		if (expParam == null) {
-		} else if (expParam.isLeaf()) {
-			filter = expParam.getLeafExpression();
-		} else if (expParam.getType() == IParam.Colon) {
-		} else {
-			for (int p = 0, psize = expParam.getSubSize(); p < psize; ++p) {
-				IParam sub = expParam.getSub(p);
-				if (sub == null) {					
-				} else if (sub.isLeaf()) {
-					filter = sub.getLeafExpression();
-					if (filter.getHome() instanceof Top) {
-						break;
-					}
-				}
-			}
-		}
-		
-		if (filter != null && filter.getHome() instanceof Top) {
-			return cursor.total(new Expression[]{filter}, ctx);
-		} else {
-			return cursor.fetch();
-		}
+		return cursor.fetch();
 	}
 }
