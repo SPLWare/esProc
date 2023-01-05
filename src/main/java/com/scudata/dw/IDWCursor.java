cdd964800d6a6d1f6a22004eeffa2b1a93bbcda7
liwei <65002112@qq.com>
2023-01-05 23:36:58 +0800

    add method to IDWCursor.

--- src/main/java/com/scudata/dw/IDWCursor.java
+++ src/main/java/com/scudata/dw/IDWCursor.java
@@ -1,5 +1,9 @@
 package com.scudata.dw;
 
+import java.io.IOException;
+
+import com.scudata.common.RQException;
+import com.scudata.dm.ObjectReader;
 import com.scudata.dm.Sequence;
 import com.scudata.dm.cursor.ICursor;
 
@@ -28,6 +32,60 @@ public abstract class IDWCursor extends ICursor {
 		super.close();
 		if (option != null && option.indexOf('x') != -1) {
 			getTableMetaData().groupTable.closeCursorEvent();
+		}
+	}
+	
+	/**
+	 * 取分段游标的起始值，如果有分段字段则返回分段字段的值，没有则返回维字段的值
+	 * @return 分段游标首条记录的分段字段的值，如果当前段数为0则返回null
+	 */
+	public Object[] getSegmentStartValues() {
+		ColPhyTable table = (ColPhyTable) getTableMetaData();
+		int startBlock = getStartBlock();
+		String segmentCol = table.getSegmentCol();
+		try {
+			if (segmentCol != null) {
+				Object[] startValues = new Object[1];
+				ObjectReader reader = table.getColumn(segmentCol).getSegmentReader();
+				for (int i = 0; i < startBlock; ++i) {
+					reader.readLong40();
+					reader.skipObject();
+					reader.skipObject();
+					reader.skipObject();
+				}
+				reader.readLong40();
+				reader.skipObject();
+				reader.skipObject();
+				startValues[0] = reader.readObject(); //startValue
+				return startValues;
+			} else {
+				ColumnMetaData[] cols = table.getSortedColumns();
+				int colCount = cols.length;
+				Object[] startValues = new Object[colCount];
+				ObjectReader []readers = new ObjectReader[colCount];
+				for (int f = 0; f < colCount; ++f) {
+					readers[f] = cols[f].getSegmentReader();
+				}
+				
+				for (int i = 0; i < startBlock; ++i) {
+					for (int f = 0; f < colCount; ++f) {
+						readers[f].readLong40();
+						readers[f].skipObject();
+						readers[f].skipObject();
+						readers[f].skipObject();
+					}
+					
+				}
+				for (int f = 0; f < colCount; ++f) {
+					readers[f].readLong40();
+					readers[f].skipObject();
+					readers[f].skipObject();
+					startValues[f] = readers[f].readObject();
+				}
+				return startValues;
+			}
+		} catch (IOException e) {
+			throw new RQException(e.getMessage(), e);
 		}
 	}
 }
