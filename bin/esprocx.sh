cd7e69906558190805fac7fc1da27c20ade0d51b
minimaomi <93764562+minimaomi@users.noreply.github.com>
2023-01-09 17:09:52 +0800

    Update esprocx.sh

--- bin/esprocx.sh
+++ bin/esprocx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /Applications/esProc.app/Contents/raqsoft/esProc/bin/setEnv.sh
-"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$START_HOME"/esProc/lib/*:"$START_HOME"/common/jdbc/*  -Duser.language=en -Dstart.home="$START_HOME"/esProc com.scudata.ide.spl.Esproc $@
+"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$START_HOME"/esProc/lib/*:"$START_HOME"/common/jdbc/*  -Duser.language=en -Dstart.home="$START_HOME"/esProc com.scudata.ide.spl.Esprocx $@
