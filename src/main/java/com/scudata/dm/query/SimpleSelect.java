27e3c542001cb05e3430ef263c2307699b73df95
xjl002 <94519665+xjl002@users.noreply.github.com>
2023-03-23 22:21:17 +0800

    from D://test/1集算器/emps.txt，去掉1后面空格

--- src/main/java/com/scudata/dm/query/SimpleSelect.java
+++ src/main/java/com/scudata/dm/query/SimpleSelect.java
@@ -3603,7 +3603,7 @@ public class SimpleSelect
 				while(pos < tableNext)
 				{
 					tableName = tableName + tokens[pos].getOriginString();
-					//xingjl 20230323  from D://test/1集算器/emps.txt，去掉1后面空格
+					//xingjl 20230323 from D://test/1集算器/emps.txt，去掉1后面空格
 					//tableName = tableName + tokens[pos].getSpaces();
 					pos++;
 				}
