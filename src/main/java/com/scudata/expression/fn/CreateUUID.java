de72c15cc427e6fd0b363aa4584aecf9f1f97899
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2023-03-29 10:15:13 +0800

    Merge branch 'master' of git@github.com:SPLWare/esProc.git

--- src/main/java/com/scudata/expression/fn/CreateUUID.java
+++ src/main/java/com/scudata/expression/fn/CreateUUID.java
@@ -17,6 +17,19 @@ public class CreateUUID extends Function {
 	}
 
 	public Object calculate(Context ctx) {
-		return UUID.randomUUID().toString();
+		UUID uuid = UUID.randomUUID();
+		long mostSigBits = uuid.getMostSignificantBits();
+		long leastSigBits = uuid.getLeastSignificantBits();
+        return (digits(mostSigBits >> 32, 8) +
+                digits(mostSigBits >> 16, 4) +
+                digits(mostSigBits, 4) +
+                digits(leastSigBits >> 48, 4) +
+                digits(leastSigBits, 12));
+		//return uuid.toString();
 	}
+	
+    private static String digits(long val, int digits) {
+        long hi = 1L << (digits * 4);
+        return Long.toHexString(hi | (val & (hi - 1))).substring(1);
+    }
 }
