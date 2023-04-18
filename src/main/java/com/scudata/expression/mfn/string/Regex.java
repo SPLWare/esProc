33f0a62d4e0ef382bec306d367d0ea05d747800b
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2023-04-18 21:31:05 +0800

    Add s.regex(rs,rpls).

--- src/main/java/com/scudata/expression/mfn/string/Regex.java
+++ src/main/java/com/scudata/expression/mfn/string/Regex.java
@@ -7,6 +7,7 @@ import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
 import com.scudata.dm.Context;
 import com.scudata.dm.Sequence;
+import com.scudata.expression.IParam;
 import com.scudata.expression.StringFunction;
 import com.scudata.resources.EngineMessage;
 
@@ -24,40 +25,76 @@ public class Regex extends StringFunction {
 			if (option.indexOf('u') != -1) flags |= Pattern.UNICODE_CASE;
 		}
 
-		if (param == null || !param.isLeaf()) {
+		if (param == null) {
+			MessageManager mm = EngineMessage.get();
+			throw new RQException("regex" + mm.getMessage("function.missingParam"));
+		} else if (param.isLeaf()) {
+			Object obj = param.getLeafExpression().calculate(ctx);
+			if (!(obj instanceof String)) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("regex" + mm.getMessage("function.paramTypeError"));
+			}
+
+			Pattern pattern = Pattern.compile((String)obj, flags);
+			Matcher m = pattern.matcher(srcStr);
+			if (!m.find()) return null;
+
+			int gcount = m.groupCount();
+			if (gcount == 0) {
+				return srcStr;
+			} else if (gcount == 1) {
+				Sequence seq = new Sequence(3);
+				do {
+					seq.add(m.group(1));
+				} while(m.find());
+				
+				return seq;
+			} else {
+				Sequence seq = new Sequence(gcount);
+				do {
+					for (int i = 1; i <= gcount; ++i) {
+						seq.add(m.group(i));
+					}
+				} while (m.find());
+				
+				return seq;
+			}
+		} else if (param.getSubSize() == 2) {
+			IParam sub0 = param.getSub(0);
+			IParam sub1 = param.getSub(1);
+			if (sub0 == null || sub1 == null) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("regex" + mm.getMessage("function.invalidParam"));
+			}
+			
+			Object obj = sub0.getLeafExpression().calculate(ctx);
+			if (!(obj instanceof String)) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("regex" + mm.getMessage("function.paramTypeError"));
+			}
+			
+			String regex = (String)obj;
+			String replacement;
+			obj = sub1.getLeafExpression().calculate(ctx);
+			if (obj instanceof String) {
+				replacement = (String)obj;
+			} else if (obj == null) {
+				replacement = "";
+			} else {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("regex" + mm.getMessage("function.paramTypeError"));
+			}
+			
+			Pattern pattern = Pattern.compile(regex, flags);
+			Matcher m = pattern.matcher(srcStr);
+			if (option == null || option.indexOf('a') == -1) {
+				return m.replaceFirst(replacement);
+			} else {
+				return m.replaceAll(replacement);
+			}
+		} else {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("regex" + mm.getMessage("function.invalidParam"));
-		}
-
-		Object obj = param.getLeafExpression().calculate(ctx);
-		if (!(obj instanceof String)) {
-			MessageManager mm = EngineMessage.get();
-			throw new RQException("regex" + mm.getMessage("function.paramTypeError"));
-		}
-
-		Pattern pattern = Pattern.compile((String)obj, flags);
-		Matcher m = pattern.matcher(srcStr);
-		if (!m.find()) return null;
-
-		int gcount = m.groupCount();
-		if (gcount == 0) {
-			return srcStr;
-		} else if (gcount == 1) {
-			Sequence seq = new Sequence(3);
-			do {
-				seq.add(m.group(1));
-			} while(m.find());
-			
-			return seq;
-		} else {
-			Sequence seq = new Sequence(gcount);
-			do {
-				for (int i = 1; i <= gcount; ++i) {
-					seq.add(m.group(i));
-				}
-			} while (m.find());
-			
-			return seq;
 		}
 	}
 }
