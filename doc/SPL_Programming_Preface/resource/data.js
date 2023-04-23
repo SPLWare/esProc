99418544d42b0b29a5349a5f4b4a9509bb76b3ee
minimaomi <294973991@qq.com>
2023-04-23 11:09:05 +0800

    update doc

--- doc/SPL_Programming_Preface/resource/data.js
+++
@@ -1,8 +0,0 @@
-var tree = new MzTreeView("tree");
-tree.setIconPath("images/");
- tree.nodes["0_1"] = "text:";
- tree.nodes["1_3"] = "text:Preface;url:topics/1.html";
- tree.nodes["1_4"] = "text:Table of contents;url:topics/2.html";
- tree.nodes["1_5"] = "text:Download PDF;url:topics/3.html";
-tree.setTarget("mainFrame");
-document.write(tree.toString());
