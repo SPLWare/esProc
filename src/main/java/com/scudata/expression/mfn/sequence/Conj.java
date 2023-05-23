c3b8136ed6aaf740e6d3c2b263c06f6db01855bc
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2023-05-23 16:24:53 +0800

    Add CS.merge() and CS.conj() function.

--- src/main/java/com/scudata/expression/mfn/sequence/Conj.java
+++ src/main/java/com/scudata/expression/mfn/sequence/Conj.java
@@ -3,6 +3,9 @@ package com.scudata.expression.mfn.sequence;
 import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
 import com.scudata.dm.Context;
+import com.scudata.dm.Sequence;
+import com.scudata.dm.cursor.ConjxCursor;
+import com.scudata.dm.cursor.ICursor;
 import com.scudata.expression.Expression;
 import com.scudata.expression.SequenceFunction;
 import com.scudata.resources.EngineMessage;
@@ -15,7 +18,29 @@ import com.scudata.resources.EngineMessage;
  */
 public class Conj extends SequenceFunction {
 	public Object calculate(Context ctx) {
-		if (param == null) {
+		if (srcSequence.ifn() instanceof ICursor) {
+			int len = srcSequence.length();
+			Sequence cursorSeq = new Sequence(len);
+			
+			for (int i = 1; i <= len; ++i) {
+				Object obj = srcSequence.getMem(i);
+				if (obj instanceof ICursor) {
+					cursorSeq.add(obj);
+				} else if (obj != null) {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("conjx" + mm.getMessage("function.paramTypeError"));
+				}
+			}
+			
+			len = cursorSeq.length();
+			if (len > 0) {
+				ICursor[] cursors = new ICursor[len];
+				cursorSeq.toArray(cursors);
+				return new ConjxCursor(cursors);
+			} else {
+				return null;
+			}
+		} else if (param == null) {
 			return srcSequence.conj(option);
 		} else if (param.isLeaf()) {
 			Expression exp = param.getLeafExpression();
--- src/main/java/com/scudata/expression/mfn/sequence/Merge.java
+++ src/main/java/com/scudata/expression/mfn/sequence/Merge.java
@@ -5,8 +5,12 @@ import com.scudata.common.RQException;
 import com.scudata.dm.Context;
 import com.scudata.dm.Sequence;
 import com.scudata.dm.cursor.ICursor;
+import com.scudata.dm.cursor.IMultipath;
 import com.scudata.dm.cursor.MemoryCursor;
+import com.scudata.dm.cursor.MultipathCursors;
+import com.scudata.dm.op.Operation;
 import com.scudata.expression.Expression;
+import com.scudata.expression.IParam;
 import com.scudata.expression.SequenceFunction;
 import com.scudata.resources.EngineMessage;
 import com.scudata.util.CursorUtil;
@@ -19,6 +23,11 @@ import com.scudata.util.CursorUtil;
  */
 public class Merge extends SequenceFunction {
 	public Object calculate(Context ctx) {
+		Sequence sequence = srcSequence;
+		if (sequence.ifn() instanceof ICursor) {
+			return mergex(sequence, param, option, ctx);
+		}
+		
 		Expression []exps;
 		if (param == null) {
 			Expression exp = new Expression("~.v()");
@@ -28,14 +37,13 @@ public class Merge extends SequenceFunction {
 		}
 		
 		if (option != null && option.indexOf('o') != -1) {
-			return srcSequence.merge(exps, option, ctx);
+			return sequence.merge(exps, option, ctx);
 		}
 		
-		Sequence seq = srcSequence;
-		int count = seq.length();
+		int count = sequence.length();
 		ICursor []cursors = new ICursor[count];
 		for (int i = 0; i < count; ++i) {
-			Object obj = seq.get(i + 1);
+			Object obj = sequence.getMem(i + 1);
 			if (obj instanceof Sequence) {
 				cursors[i] = new MemoryCursor((Sequence)obj);
 			} else if (obj != null) {
@@ -48,5 +56,90 @@ public class Merge extends SequenceFunction {
 		
 		ICursor cursor = CursorUtil.merge(cursors, exps, option, ctx);
 		return cursor.fetch();
+	}
+	
+	private static Object mergex(Sequence sequence, IParam param, String opt, Context ctx) {
+		int srcLen = sequence.length();
+		int count = srcLen;
+		for (int i = 1; i <= srcLen; ++i) {
+			Object obj = sequence.getMem(i);
+			if (obj == null) {
+				count--;
+			} else if (!(obj instanceof ICursor)) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("\".\"" + mm.getMessage("dot.cursorLeft"));
+			}
+		}
+		
+		if (count < 1) {
+			return null;
+		} else if (count == 1) {
+			return sequence.ifn();
+		}
+
+		ICursor []cursors = new ICursor[count];
+		boolean isMultipath = false;
+		int pathCount = 1;
+
+		for (int i = 1, q = 0; i <= srcLen; ++i) {
+			Object obj = sequence.getMem(i);
+			if (obj != null) {
+				ICursor cursor = (ICursor)obj;
+				cursors[q++] = cursor;
+				
+				if (cursor instanceof IMultipath) {
+					if (q == 1) {
+						isMultipath = true;
+						pathCount = ((IMultipath)cursor).getPathCount();
+					} else if (pathCount != ((IMultipath)cursor).getPathCount()) {
+						isMultipath = false;
+					}
+				}
+			}
+		}
+
+		Expression []exps = null;
+		if (param == null) {
+		} else if (param.isLeaf()) { // 只有一个参数
+			exps = new Expression[]{ param.getLeafExpression() };
+		} else if (param.getType() == IParam.Comma) { // ,
+			int size = param.getSubSize();
+			exps = new Expression[size];
+			for (int i = 0; i < size; ++i) {
+				IParam sub = param.getSub(i);
+				if (sub == null || !sub.isLeaf()) {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("mergex" + mm.getMessage("function.invalidParam"));
+				}
+				exps[i] = sub.getLeafExpression();
+			}
+		} else {
+			MessageManager mm = EngineMessage.get();
+			throw new RQException("mergex" + mm.getMessage("function.invalidParam"));
+		}
+
+		if (isMultipath && pathCount > 1) {
+			ICursor []result = new ICursor[pathCount];
+			ICursor [][]multiCursors = new ICursor[count][];
+			for (int i = 0; i < count; ++i) {
+				IMultipath multipath = (IMultipath)cursors[i];
+				multiCursors[i] = multipath.getParallelCursors();
+			}
+			
+			for (int i = 0; i < pathCount; ++i) {
+				ICursor []curs = new ICursor[count];
+				for (int c = 0; c < count; ++c) {
+					curs[c] = multiCursors[c][i];
+				}
+
+				Context tmpCtx = ctx.newComputeContext();
+				Expression []tmpExps = Operation.dupExpressions(exps, tmpCtx);
+				result[i] = CursorUtil.merge(curs, tmpExps, opt, tmpCtx);
+			}
+			
+			return new MultipathCursors(result, ctx);
+		} else {
+			return CursorUtil.merge(cursors, exps, opt, ctx);
+		}
 	}
 }
