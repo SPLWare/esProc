cb7e8dc343b4ef73557febd49ec0dc2e7ff3a987
liwei <65002112@qq.com>
2023-07-21 17:57:40 +0800

    Fix bug of index cursor.

--- src/main/java/com/scudata/dw/IndexCursor.java
+++ src/main/java/com/scudata/dw/IndexCursor.java
@@ -24,7 +24,7 @@ import com.scudata.resources.EngineMessage;
  *
  */
 public class IndexCursor extends ICursor {
-	private int BUFFER_SIZE = 4096 * 4;
+	private int BUFFER_SIZE = 1024 * 32;
 	
 	private PhyTable table;
 	private String []fields;//È¡³ö×Ö¶Î
