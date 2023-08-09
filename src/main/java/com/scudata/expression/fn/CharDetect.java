6f8e504c12ac6e119f4af8bead2676b23c7ceb9c
liaoxch <liaoxinchuan@raqsoft.com>
2023-08-09 14:48:05 +0800

    改进编码识别

--- src/main/java/com/scudata/expression/fn/CharDetect.java
+++ src/main/java/com/scudata/expression/fn/CharDetect.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -147,6 +148,12 @@ public class CharDetect extends CharFunction {
         
 	    if (encoding ==null){
 	    	encoding = CharEncodingDetectEx.getJavaEncode(obj);
+	    }else{
+	    	BytesEncodingDetect s = new BytesEncodingDetect(); 
+	    	List<String> list= Arrays.asList(EncodingEx.javaname);
+    		if(list.indexOf(encoding)==-1){
+    			encoding = CharEncodingDetectEx.getJavaEncode(obj);
+    		}	    	
 	    }
 	    
 	    if (m_codes!=null){	
