07253d17efeb6f49b83c1b0e670defe863aab9fc
wunanraq <wunan@raqsoft.com>
2023-08-17 16:00:02 +0800

    When parameter B is missing, it is calculated as a zero-vector

--- src/main/java/com/scudata/expression/fn/algebra/Distance.java
+++ src/main/java/com/scudata/expression/fn/algebra/Distance.java
@@ -24,10 +24,22 @@ public class Distance extends Function {
 	}
 	
 	public Object calculate(Context ctx) {
+		boolean manhattan = false;
+		boolean standard = false;
+		if (option != null) {
+			if (option.indexOf('a') > -1) {
+				manhattan = true;
+			}
+			if (option.indexOf('m') > -1) {
+				standard = true;
+			}
+		}
+		Sequence s1 = null;
+		Sequence s2 = null;
 		if (param.isLeaf()) {
 			Object o = param.getLeafExpression().calculate(ctx);
 			if (o instanceof Sequence) {
-				return o;
+				s1 = (Sequence) o;
 			} else {
 				MessageManager mm = EngineMessage.get();
 				throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
@@ -48,69 +60,68 @@ public class Distance extends Function {
 			Object o2 = sub2.getLeafExpression().calculate(ctx);
 			if (o1 instanceof Sequence && o2 instanceof Sequence) {
 				// edited by bd, 2021.11.17, 在dis函数中，单层序列认为是横向量
-				Sequence s1 = (Sequence) o1;
-				Sequence s2 = (Sequence) o2;
-				Matrix A = new Matrix(s1);
-				Matrix B = new Matrix(s2);
-				
-				Object o11 = s1.length() > 0 ? s1.get(1) : null;
+				s1 = (Sequence) o1;
+				s2 = (Sequence) o2;
+			}
+		}
+		if (s1 != null) {
+			Matrix A = new Matrix(s1);
+			Object o11 = s1.length() > 0 ? s1.get(1) : null;
+			if (!(o11 instanceof Sequence)) {
+				// A为单序列定义的向量，转成横向量
+				A = A.transpose();
+			}
+			Matrix B = null;
+			if (s2 != null) {
+				B = new Matrix(s2);
 				Object o21 = s2.length() > 0 ? s2.get(1) : null;
-				if (!(o11 instanceof Sequence)) {
-					// A为单序列定义的向量，转成横向量
-					A = A.transpose();
-				}
 				if (!(o21 instanceof Sequence)) {
 					// A为单序列定义的向量，转成横向量
 					B = B.transpose();
 				}
-				
-				if (A.getCols() == 0 && A.getRows() == 0) {
-					MessageManager mm = EngineMessage.get();
-					throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
-				}
-				else if (B.getCols() == 0 && B.getRows() == 0) {
-					MessageManager mm = EngineMessage.get();
-					throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
-				}
-				Matrix X = new Matrix(A.getRows(), B.getRows());
-				double[][] xs = X.getArray();
-				boolean manhattan = false;
-				boolean standard = false;
-				if (option != null) {
-					if (option.indexOf('a') > -1) {
-						manhattan = true;
-					}
-					if (option.indexOf('m') > -1) {
-						standard = true;
-					}
-				}
-				for (int r = 0; r < A.getRows(); r++) {
-					for (int c = 0; c < B.getRows(); c++) {
-						double res = 0;
-						for (int i = 0; i < A.getCols(); i++) {
-							if (manhattan) {
-								res += Math.abs(A.get(r, i)-B.get(c, i));
-							}
-							else {
-								res += Math.pow((A.get(r, i) - B.get(c, i)), 2);
-							}
-						}
-						if (standard) {
-							res = res / A.getCols();
-						}
-						if (manhattan) {
-							xs[r][c] = res;
-						}
-						else {
-							xs[r][c] = Math.sqrt(res);
-						}
-					}
-				}
-				return X.toSequence(option, false);
-			} else {
+			}
+			else {
+				double[][] bs = new double[1][A.getCols()];
+				B = new Matrix(bs);
+			}
+			
+			if (A.getCols() == 0 && A.getRows() == 0) {
 				MessageManager mm = EngineMessage.get();
 				throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
 			}
+			else if (B.getCols() == 0 && B.getRows() == 0) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
+			}
+			Matrix X = new Matrix(A.getRows(), B.getRows());
+			double[][] xs = X.getArray();
+			for (int r = 0; r < A.getRows(); r++) {
+				for (int c = 0; c < B.getRows(); c++) {
+					double res = 0;
+					for (int i = 0; i < A.getCols(); i++) {
+						if (manhattan) {
+							res += Math.abs(A.get(r, i)-B.get(c, i));
+						}
+						else {
+							res += Math.pow((A.get(r, i) - B.get(c, i)), 2);
+						}
+					}
+					if (standard) {
+						res = res / A.getCols();
+					}
+					if (manhattan) {
+						xs[r][c] = res;
+					}
+					else {
+						xs[r][c] = Math.sqrt(res);
+					}
+				}
+			}
+			return X.toSequence(option, false);
+		} 
+	    else {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("dis" + mm.getMessage("function.paramTypeError"));
 		}
 	}
 
