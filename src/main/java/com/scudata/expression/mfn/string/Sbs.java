d4e05d55151a0d88be5e12d92a263ab1dda4a317
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2024-02-27 14:09:52 +0800

    Modify sbs function.

--- src/main/java/com/scudata/expression/mfn/string/Sbs.java
+++ src/main/java/com/scudata/expression/mfn/string/Sbs.java
@@ -14,6 +14,78 @@ import com.scudata.resources.EngineMessage;
  *
  */
 public class Sbs extends StringFunction {
+	private static String substring(String srcStr, int n) {
+		int len = srcStr.length();
+		if (n > len) {
+			return "";
+		} else if (n > 0) {
+			return srcStr.substring(n - 1, n);
+		} else if (n < 0) {
+			n += len;
+			if (n < 0) {
+				return "";
+			} else {
+				return srcStr.substring(n, n + 1);
+			}
+		} else {
+			MessageManager mm = EngineMessage.get();
+			throw new RQException("sbs" + mm.getMessage("function.invalidParam"));
+		}
+	}
+	
+	private static String substring(String srcStr, IParam startParam, IParam endParam, Context ctx) {
+		int len = srcStr.length();
+		int start = 0;
+		if (startParam != null) {
+			Object obj = startParam.getLeafExpression().calculate(ctx);
+			if (!(obj instanceof Number)) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
+			}
+			
+			start = ((Number)obj).intValue();
+			if (start > len) {
+				return "";
+			} else if (start < 0) {
+				start += len;
+				if (start < 0) {
+					start = 0;
+				}
+			} else if (start == 0) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sbs" + mm.getMessage("function.invalidParam"));
+			} else {
+				start--;
+			}
+		}
+		
+		if (endParam != null) {
+			Object obj = endParam.getLeafExpression().calculate(ctx);
+			if (!(obj instanceof Number)) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
+			}
+			
+			int end  = ((Number)obj).intValue();
+			if (end > len) {
+				return srcStr.substring(start);
+			} else if (end < 0) {
+				end += len + 1;
+			} else if (end == 0) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sbs" + mm.getMessage("function.invalidParam"));
+			}
+			
+			if (start < end) {
+				return srcStr.substring(start, end);
+			} else {
+				return "";
+			}
+		} else {
+			return srcStr.substring(start);
+		}
+	}
+	
 	public Object calculate(Context ctx) {
 		if (param == null) {
 			MessageManager mm = EngineMessage.get();
@@ -25,39 +97,14 @@ public class Sbs extends StringFunction {
 				throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
 			}
 			
-			int n = ((Number)obj).intValue();
-			return srcStr.substring(n - 1, n);
+			return substring(srcStr, ((Number)obj).intValue());
 		} else if (param.getType() == IParam.Colon) { // :
 			if (param.getSubSize() != 2) {
 				MessageManager mm = EngineMessage.get();
 				throw new RQException("sbs" + mm.getMessage("function.invalidParam"));
 			}
 			
-			int start = 0;
-			IParam sub0 = param.getSub(0);
-			if (sub0 != null) {
-				Object obj = sub0.getLeafExpression().calculate(ctx);
-				if (!(obj instanceof Number)) {
-					MessageManager mm = EngineMessage.get();
-					throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
-				}
-				
-				start = ((Number)obj).intValue() - 1;
-			}
-			
-			IParam sub1 = param.getSub(1);
-			if (sub1 != null) {
-				Object obj = sub1.getLeafExpression().calculate(ctx);
-				if (!(obj instanceof Number)) {
-					MessageManager mm = EngineMessage.get();
-					throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
-				}
-				
-				int end  = ((Number)obj).intValue();
-				return srcStr.substring(start, end);
-			} else {
-				return srcStr.substring(start);
-			}
+			return substring(srcStr, param.getSub(0), param.getSub(1), ctx);
 		} else if (param.getType() == IParam.Comma) { // ,
 			String result = "";
 			for (int i = 0, size = param.getSubSize(); i < size; ++i) {
@@ -72,39 +119,14 @@ public class Sbs extends StringFunction {
 						throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
 					}
 					
-					int n = ((Number)obj).intValue();
-					result += srcStr.substring(n - 1, n);
+					result += substring(srcStr, ((Number)obj).intValue());
 				} else {
 					if (sub.getSubSize() != 2) {
 						MessageManager mm = EngineMessage.get();
 						throw new RQException("sbs" + mm.getMessage("function.invalidParam"));
 					}
 					
-					int start = 0;
-					IParam sub0 = sub.getSub(0);
-					if (sub0 != null) {
-						Object obj = sub0.getLeafExpression().calculate(ctx);
-						if (!(obj instanceof Number)) {
-							MessageManager mm = EngineMessage.get();
-							throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
-						}
-						
-						start = ((Number)obj).intValue() - 1;
-					}
-					
-					IParam sub1 = sub.getSub(1);
-					if (sub1 != null) {
-						Object obj = sub1.getLeafExpression().calculate(ctx);
-						if (!(obj instanceof Number)) {
-							MessageManager mm = EngineMessage.get();
-							throw new RQException("sbs" + mm.getMessage("function.paramTypeError"));
-						}
-						
-						int end  = ((Number)obj).intValue();
-						result += srcStr.substring(start, end);
-					} else {
-						result += srcStr.substring(start);
-					}
+					result += substring(srcStr, sub.getSub(0), sub.getSub(1), ctx);
 				}
 			}
 			
