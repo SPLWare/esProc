08b2935aa802a610abc9c0f2aa613e65ca5ab237
minimaomi <294973991@qq.com>
2024-06-06 13:52:39 +0800

    20240602

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /raqsoft/esProc/bin/setEnv.sh
-"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$START_HOME"/esProc/lib/*:"$START_HOME"/common/jdbc/* -Duser.language="$language" -Dstart.home="$START_HOME"/esProc  com.scudata.ide.spl.SPL
+"$EXEC_JAVA" $(jvm_args=$(grep '^jvm_args=' "$START_HOME"/esProc/bin/config.txt | cut -d'=' -f2)
+echo "$jvm_args") -cp "$START_HOME"/esProc/classes:"$START_HOME"/esProc/lib/*:"$START_HOME"/common/jdbc/* -Duser.language="$language" -Dstart.home="$START_HOME"/esProc  com.scudata.ide.spl.SPL
