6aea9cd076076fafd3222626c4e115fd42e40468
hdw <460778537@qq.com>
2024-07-18 18:59:23 +0800

    add setAllowedClassName method to FilteredObjectInputStream

--- src/main/java/com/scudata/common/FilteredObjectInputStream.java
+++ src/main/java/com/scudata/common/FilteredObjectInputStream.java
@@ -6,15 +6,13 @@ import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectStreamClass;
 
-import com.scudata.ide.custom.server.ServerAsk;
-
 public class FilteredObjectInputStream extends ObjectInputStream{
 	
 	private static final byte STATUS_ALLOWED = 1;
 	private static final byte STATUS_REJECTED = 0;
 	private static final byte STATUS_UNDECIDED = -1;
 	private byte status = STATUS_UNDECIDED;//the status of the top layer class being validated
-
+	private String allowedClassName = null;
 	public FilteredObjectInputStream(InputStream in) throws IOException {
 		super(in);
 	}
@@ -26,11 +24,17 @@ public class FilteredObjectInputStream extends ObjectInputStream{
 	
 	@Override
 	protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
-		if(status == STATUS_UNDECIDED && !desc.getName().equals(ServerAsk.class.getName())) {
+		if(allowedClassName != null && status == STATUS_UNDECIDED && !desc.getName().equals(allowedClassName)) {
 			status = STATUS_REJECTED;
 			throw new InvalidClassException("Unauthorized deserialization attempt", desc.getName());
 		}
 		status = STATUS_ALLOWED;
 		return super.resolveClass(desc);
 	}
+
+	public void setAllowedClassName(String allowedClassName) {
+		this.allowedClassName = allowedClassName;
+	}
+	
+	
 }
