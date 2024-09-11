a8fd0c47e7d9810930403d3f5989140cd5c2dee4
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2024-09-11 23:22:45 +0800

    Modify pdate function.

--- src/main/java/com/scudata/common/DateFactory.java
+++ src/main/java/com/scudata/common/DateFactory.java
@@ -9,6 +9,19 @@ import java.util.Date;
  * 线程安全类，但每次格式化和分析前需设置当前的格式串，否则上次的设置会影响下次的格式化和分析
  */
 public class DateFactory {
+	// 每月的天数
+	public static final int []DAYS 			= new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
+	public static final int []LEEPYEARDAYS 	= new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
+	
+	// 1900年到2100年每年1月1号的星期数，星期日的值是1
+	private static final int WEEK_START_YEAR = 1900;
+	private static final int WEEK_END_YEAR = 2100;
+	private static final int []WEEKS = new int[] {2,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7
+			,1,3 ,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7
+			,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7
+			,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7
+			,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6,1,2,3,4,6,7,1,2,4,5,6,7,2,3,4,5,7,1,2,3,5,6,7,1,3,4,5,6};
+
 	private static ThreadLocal<DateFactory> local = new ThreadLocal<DateFactory>() {
 		protected synchronized DateFactory initialValue() {
 			return new DateFactory();
@@ -63,6 +76,11 @@ public class DateFactory {
 		return new java.sql.Time(gc.getTimeInMillis());
 	}
 
+	/**
+	 * 取给定日期所在周的第一天，星期日为第一天
+	 * @param date
+	 * @return Date
+	 */
 	public Date weekBegin(Date date) {
 		Calendar gc = getCalendar();
 		gc.setFirstDayOfWeek(Calendar.SUNDAY);
@@ -76,9 +94,9 @@ public class DateFactory {
 	}
 	
 	/**
-	 * 以周一为第一天
+	 * 取给定日期所在周的第一天，星期一为第一天
 	 * @param date
-	 * @return
+	 * @return Date
 	 */
 	public Date weekBegin1(Date date) {
 		Calendar gc = getCalendar();
@@ -197,6 +215,10 @@ public class DateFactory {
 		gc.setTime(date);
 		gc.set(Calendar.MONTH, Calendar.JANUARY);
 		gc.set(Calendar.DAY_OF_MONTH, 1);
+		gc.set(Calendar.HOUR_OF_DAY, 0);
+		gc.set(Calendar.MINUTE, 0);
+		gc.set(Calendar.SECOND, 0);
+		gc.set(Calendar.MILLISECOND, 0);
 		return new java.sql.Date(gc.getTimeInMillis());
 	}
 	
@@ -206,6 +228,10 @@ public class DateFactory {
 		gc.setTime(date);
 		gc.set(Calendar.MONTH, Calendar.DECEMBER);
 		gc.set(Calendar.DAY_OF_MONTH, 31);
+		gc.set(Calendar.HOUR_OF_DAY, 0);
+		gc.set(Calendar.MINUTE, 0);
+		gc.set(Calendar.SECOND, 0);
+		gc.set(Calendar.MILLISECOND, 0);
 		return new java.sql.Date(gc.getTimeInMillis());
 	}
 
@@ -499,5 +525,428 @@ public class DateFactory {
 		} else {
 			return days % 32;
 		}
+	}
+	
+	private int weekBign(int y, int m, int d) {
+		if (y < WEEK_START_YEAR || y > WEEK_END_YEAR) {
+			Calendar gc = getCalendar();
+			gc.setFirstDayOfWeek(Calendar.SUNDAY);
+			gc.set(y, m, d, 0, 0, 0);
+			gc.set(Calendar.MILLISECOND, 0);
+			gc.getTimeInMillis(); // 更新日期后再修改星期
+			
+			gc.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+			y = calendar.get(Calendar.YEAR);
+			m = calendar.get(Calendar.MONTH);
+			d = calendar.get(Calendar.DAY_OF_MONTH);
+		} else if (m == 0) {
+			// 计算指定日期是星期几
+			int w = WEEKS[y - WEEK_START_YEAR] + (d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+			
+			if (d < 1) {
+				y--;
+				m = 11;
+				d = 31 + d;
+			}
+		} else if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+			// 闰年
+			int w = WEEKS[y - WEEK_START_YEAR];
+			int days = 31;
+			for (int i = 1; i < m; ++i) {
+				days += LEEPYEARDAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+
+			if (d < 1) {
+				m--;
+				d = LEEPYEARDAYS[m] + d;
+			}
+		} else {
+			int w = WEEKS[y - WEEK_START_YEAR];
+			int days = 31;
+			for (int i = 1; i < m; ++i) {
+				days += DAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+
+			if (d < 1) {
+				m--;
+				d = DAYS[m] + d;
+			}
+		}
+		
+		return toDays(y, m, d);
+	}
+	
+	private int weekEnd(int y, int m, int d) {
+		if (y < WEEK_START_YEAR || y > WEEK_END_YEAR) {
+			Calendar gc = getCalendar();
+			gc.setFirstDayOfWeek(Calendar.SUNDAY);
+			gc.set(y, m, d, 0, 0, 0);
+			gc.set(Calendar.MILLISECOND, 0);
+			gc.getTimeInMillis(); // 更新日期后再修改星期
+			
+			gc.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+			y = calendar.get(Calendar.YEAR);
+			m = calendar.get(Calendar.MONTH);
+			d = calendar.get(Calendar.DAY_OF_MONTH);
+		} else if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+			// 闰年
+			int w = WEEKS[y - WEEK_START_YEAR];
+			int days = 0;
+			for (int i = 0; i < m; ++i) {
+				days += LEEPYEARDAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d += (14 - w);
+			} else {
+				d += (7 - w);
+			}
+
+			if (d > LEEPYEARDAYS[m]) {
+				d -= LEEPYEARDAYS[m];
+				m++;
+			}
+		} else {
+			int w = WEEKS[y - WEEK_START_YEAR];
+			int days = 0;
+			for (int i = 0; i < m; ++i) {
+				days += DAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d += (14 - w);
+			} else {
+				d += (7 - w);
+			}
+
+			if (d > DAYS[m]) {
+				d -= DAYS[m];
+				m++;
+			}
+		}
+		
+		return toDays(y, m, d);
+	}
+	
+	private int weekBign1(int y, int m, int d) {
+		if (y < WEEK_START_YEAR || y > WEEK_END_YEAR) {
+			Calendar gc = getCalendar();
+			gc.setFirstDayOfWeek(Calendar.MONDAY);
+			gc.set(y, m, d, 0, 0, 0);
+			gc.set(Calendar.MILLISECOND, 0);
+			gc.getTimeInMillis(); // 更新日期后再修改星期
+			
+			gc.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+			y = calendar.get(Calendar.YEAR);
+			m = calendar.get(Calendar.MONTH);
+			d = calendar.get(Calendar.DAY_OF_MONTH);
+		} else if (m == 0) {
+			// 计算指定日期是星期几
+			int w = WEEKS[y - WEEK_START_YEAR] - 1;
+			if (w == 0) {
+				w = 7;
+			}
+			
+			w += (d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+			
+			if (d < 1) {
+				y--;
+				m = 11;
+				d = 31 + d;
+			}
+		} else if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+			// 闰年
+			int w = WEEKS[y - WEEK_START_YEAR] - 1;
+			if (w == 0) {
+				w = 7;
+			}
+			
+			int days = 31;
+			for (int i = 1; i < m; ++i) {
+				days += LEEPYEARDAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+
+			if (d < 1) {
+				m--;
+				d = LEEPYEARDAYS[m] + d;
+			}
+		} else {
+			int w = WEEKS[y - WEEK_START_YEAR] - 1;
+			if (w == 0) {
+				w = 7;
+			}
+			
+			int days = 31;
+			for (int i = 1; i < m; ++i) {
+				days += DAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d -= (w - 8);
+			} else {
+				d -= (w - 1);
+			}
+
+			if (d < 1) {
+				m--;
+				d = DAYS[m] + d;
+			}
+		}
+		
+		return toDays(y, m, d);
+	}
+	
+	private int weekEnd1(int y, int m, int d) {
+		if (y < WEEK_START_YEAR || y > WEEK_END_YEAR) {
+			Calendar gc = getCalendar();
+			gc.setFirstDayOfWeek(Calendar.MONDAY);
+			gc.set(y, m, d, 0, 0, 0);
+			gc.set(Calendar.MILLISECOND, 0);
+			gc.getTimeInMillis(); // 更新日期后再修改星期
+			
+			gc.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+			y = calendar.get(Calendar.YEAR);
+			m = calendar.get(Calendar.MONTH);
+			d = calendar.get(Calendar.DAY_OF_MONTH);
+		} else if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+			// 闰年
+			int w = WEEKS[y - WEEK_START_YEAR] - 1;
+			if (w == 0) {
+				w = 7;
+			}
+			
+			int days = 0;
+			for (int i = 0; i < m; ++i) {
+				days += LEEPYEARDAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d += (14 - w);
+			} else {
+				d += (7 - w);
+			}
+
+			if (d > LEEPYEARDAYS[m]) {
+				d -= LEEPYEARDAYS[m];
+				m++;
+			}
+		} else {
+			int w = WEEKS[y - WEEK_START_YEAR] - 1;
+			if (w == 0) {
+				w = 7;
+			}
+			
+			int days = 0;
+			for (int i = 0; i < m; ++i) {
+				days += DAYS[i];
+			}
+			
+			w += (days + d - 1) % 7;
+			if (w > 7) {
+				d += (14 - w);
+			} else {
+				d += (7 - w);
+			}
+
+			if (d > DAYS[m]) {
+				d -= DAYS[m];
+				m++;
+			}
+		}
+		
+		return toDays(y, m, d);
+	}
+	
+	/**
+	 * 取给定日期所在周的第一天，星期日为第一天
+	 * @param days
+	 * @return int
+	 */
+	public int weekBegin(int days) {
+		if (days < 0) {
+			return weekBign(days / 384 + 1969, days / 32 % 12 + 11, days % 32 + 32);
+		} else {
+			return weekBign(days / 384 + 1970, days / 32 % 12, days % 32);
+		}
+	}
+	
+	/**
+	 * 取给定日期所在周的第一天，星期一为第一天
+	 * @param days
+	 * @return int
+	 */
+	public int weekBegin1(int days) {
+		if (days < 0) {
+			return weekBign1(days / 384 + 1969, days / 32 % 12 + 11, days % 32 + 32);
+		} else {
+			return weekBign1(days / 384 + 1970, days / 32 % 12, days % 32);
+		}
+	}
+
+	/**
+	 * 取给定日期所在周的最后一天，星期日为第一天
+	 * @param days
+	 * @return int
+	 */
+	public int weekEnd(int days) {
+		if (days < 0) {
+			return weekEnd(days / 384 + 1969, days / 32 % 12 + 11, days % 32 + 32);
+		} else {
+			return weekEnd(days / 384 + 1970, days / 32 % 12, days % 32);
+		}
+	}
+	
+	/**
+	 * 取给定日期所在周的最后一天，星期一为第一天
+	 * @param days
+	 * @return int
+	 */
+	public int weekEnd1(int days) {
+		if (days < 0) {
+			return weekEnd1(days / 384 + 1969, days / 32 % 12 + 11, days % 32 + 32);
+		} else {
+			return weekEnd1(days / 384 + 1970, days / 32 % 12, days % 32);
+		}
+	}
+	
+	/**
+	 * 取指定日期的月首日期
+	 * @param days 指定的日期
+	 * @return 月首日期
+	 */
+	public int monthBegin(int days) {
+		if (days < 0) {
+			return toDays(days / 384 + 1969, days / 32 % 12 + 11, 1);
+		} else {
+			return toDays(days / 384 + 1970, days / 32 % 12, 1);
+		}
+	}
+	
+	/**
+	 * 取指定日期的月末日期
+	 * @param days 指定的日期
+	 * @return 月末日期
+	 */
+	public int monthEnd(int days) {
+		if (days < 0) {
+			int y = days / 384 + 1969;
+			int m = days / 32 % 12 + 11;
+			if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+				return toDays(y, m, LEEPYEARDAYS[m]);
+			} else {
+				return toDays(y, m, DAYS[m]);
+			}
+		} else {
+			int y = days / 384 + 1970;
+			int m = days / 32 % 12;
+			if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+				return toDays(y, m, LEEPYEARDAYS[m]);
+			} else {
+				return toDays(y, m, DAYS[m]);
+			}
+		}
+	}
+	
+	/**
+	 * 取指定日期所在季度的第一天
+	 * @param days
+	 * @return int
+	 */
+	public int quaterBegin(int days) {
+		if (days < 0) {
+			int m = ((days / 32 % 12 + 11) / 3) * 3;
+			return toDays(days / 384 + 1969, m, 1);
+		} else {
+			int m = ((days / 32 % 12) / 3) * 3;
+			return toDays(days / 384 + 1970, m, 1);
+		}		
+	}
+	
+	/**
+	 * 取指定日期所在季度的最后一天
+	 * @param days
+	 * @return int
+	 */
+	public int quaterEnd(int days) {
+		if (days < 0) {
+			int y = days / 384 + 1969;
+			int m = ((days / 32 % 12 + 11) / 3) * 3 + 2;
+			
+			if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+				return toDays(y, m, LEEPYEARDAYS[m]);
+			} else {
+				return toDays(y, m, DAYS[m]);
+			}
+		} else {
+			int y = days / 384 + 1970;
+			int m = ((days / 32 % 12) / 3) * 3 + 2;
+			
+			if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) {
+				return toDays(y, m, LEEPYEARDAYS[m]);
+			} else {
+				return toDays(y, m, DAYS[m]);
+			}
+		}		
+	}
+
+	/**
+	 * 取指定日期所在年的第一天
+	 * @param days
+	 * @return int
+	 */
+	public int yearBegin(int days) {
+		if (days < 0) {
+			return toDays(days / 384 + 1969, 0, 1);
+		} else {
+			return toDays(days / 384 + 1970, 0, 1);
+		}		
+	}
+	
+	/**
+	 * 取指定日期所在年的最后一天
+	 * @param days
+	 * @return int
+	 */
+	public int yearEnd(int days) {
+		if (days < 0) {
+			return toDays(days / 384 + 1969, 11, 31);
+		} else {
+			return toDays(days / 384 + 1970, 11, 31);
+		}		
 	}
 }
--- src/main/java/com/scudata/expression/fn/datetime/PDate.java
+++ src/main/java/com/scudata/expression/fn/datetime/PDate.java
@@ -2,6 +2,11 @@ package com.scudata.expression.fn.datetime;
 
 import java.util.Date;
 
+import com.scudata.array.ConstArray;
+import com.scudata.array.IArray;
+import com.scudata.array.IntArray;
+import com.scudata.array.NumberArray;
+import com.scudata.array.ObjectArray;
 import com.scudata.common.DateFactory;
 import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
@@ -29,56 +34,223 @@ public class PDate extends Function {
 		}
 	}
 
-	public Object calculate(Context ctx) {
-		Object result = param.getLeafExpression().calculate(ctx);
+	private static Object pdate(Object result, String option) {
 		if (result == null) {
 			return null;
 		} else if (result instanceof String) {
 			result = Variant.parseDate((String)result);
 		}
+		
+		if (result instanceof Date) {
+			Date date = (Date)result;
+			if (option != null) {
+				if (option.indexOf('w') != -1) {
+					if (option.indexOf('e') == -1) {
+						if (option.indexOf('1') == -1) {
+							return DateFactory.get().weekBegin(date);
+						} else {
+							return DateFactory.get().weekBegin1(date);
+						}
+					} else {
+						if (option.indexOf('1') == -1) {
+							return DateFactory.get().weekEnd(date);
+						} else {
+							return DateFactory.get().weekEnd1(date);
+						}
+					}
+				} else if (option.indexOf('m') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().monthBegin(date);
+					} else {
+						return DateFactory.get().monthEnd(date);
+					}
+				} else if (option.indexOf('q') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().quaterBegin(date);
+					} else {
+						return DateFactory.get().quaterEnd(date);
+					}
+				} else if (option.indexOf('y') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().yearBegin(date);
+					} else {
+						return DateFactory.get().yearEnd(date);
+					}
+				}
+			}
 
-		if (!(result instanceof Date)) {
+			return DateFactory.get().weekBegin(date);
+		} else if (result instanceof Number) {
+			int date = ((Number)result).intValue();
+			if (option != null) {
+				if (option.indexOf('w') != -1) {
+					if (option.indexOf('e') == -1) {
+						if (option.indexOf('1') == -1) {
+							return DateFactory.get().weekBegin(date);
+						} else {
+							return DateFactory.get().weekBegin1(date);
+						}
+					} else {
+						if (option.indexOf('1') == -1) {
+							return DateFactory.get().weekEnd(date);
+						} else {
+							return DateFactory.get().weekEnd1(date);
+						}
+					}
+				} else if (option.indexOf('m') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().monthBegin(date);
+					} else {
+						return DateFactory.get().monthEnd(date);
+					}
+				} else if (option.indexOf('q') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().quaterBegin(date);
+					} else {
+						return DateFactory.get().quaterEnd(date);
+					}
+				} else if (option.indexOf('y') != -1) {
+					if (option.indexOf('e') == -1) {
+						return DateFactory.get().yearBegin(date);
+					} else {
+						return DateFactory.get().yearEnd(date);
+					}
+				}
+			}
+
+			return DateFactory.get().weekBegin(date);
+		} else {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("pdate" + mm.getMessage("function.paramTypeError"));
 		}
-
-		Date date = (Date)result;
-		if (option != null) {
-			if (option.indexOf('w') != -1) {
-				if (option.indexOf('e') == -1) {
-					if (option.indexOf('1') == -1) {
-						return DateFactory.get().weekBegin(date);
-					} else {
-						return DateFactory.get().weekBegin1(date);
-					}
+	}
+	
+	public Object calculate(Context ctx) {
+		Object result = param.getLeafExpression().calculate(ctx);
+		return pdate(result, option);
+	}
+	
+	/**
+	 * 计算出所有行的结果
+	 * @param ctx 计算上行文
+	 * @return IArray
+	 */
+	public IArray calculateAll(Context ctx) {
+		IArray array = param.getLeafExpression().calculateAll(ctx);
+		int size = array.size();
+		String option = this.option;
+		
+		if (array instanceof ConstArray) {
+			Object result = array.get(1);
+			if (result == null) {
+				return new ConstArray(null, size);
+			} else if (result instanceof String) {
+				result = Variant.parseDate((String)result);
+			}
+			
+			result = pdate(result, option);
+			return new ConstArray(result, size);
+		} else if (array instanceof NumberArray) {
+			IntArray result = new IntArray(size);
+			result.setTemporary(true);
+			DateFactory df = DateFactory.get();
+			boolean isEnd = false, isMondayFirst = false, isMonth = false, isQuater = false, isYear = false;
+			
+			if (option != null) {
+				if (option.indexOf('e') != -1) isEnd = true;
+				
+				if (option.indexOf('m') != -1) {
+					isMonth = true;
+				} else if (option.indexOf('q') != -1) {
+					isQuater = true;
+				} else if (option.indexOf('y') != -1) {
+					isYear = true;
 				} else {
-					if (option.indexOf('1') == -1) {
-						return DateFactory.get().weekEnd(date);
-					} else {
-						return DateFactory.get().weekEnd1(date);
-					}
-				}
-			} else if (option.indexOf('m') != -1) {
-				if (option.indexOf('e') == -1) {
-					return DateFactory.get().monthBegin(date);
-				} else {
-					return DateFactory.get().monthEnd(date);
-				}
-			} else if (option.indexOf('q') != -1) {
-				if (option.indexOf('e') == -1) {
-					return DateFactory.get().quaterBegin(date);
-				} else {
-					return DateFactory.get().quaterEnd(date);
-				}
-			} else if (option.indexOf('y') != -1) {
-				if (option.indexOf('e') == -1) {
-					return DateFactory.get().yearBegin(date);
-				} else {
-					return DateFactory.get().yearEnd(date);
+					if (option.indexOf('1') != -1) isMondayFirst = true;
 				}
 			}
+			
+			if (isMonth) {
+				for (int i = 1; i <= size; ++i) {
+					if (array.isNull(i)) {
+						result.pushNull();
+					} else {
+						int date;
+						if (isEnd) {
+							date = df.monthEnd(array.getInt(i));
+						} else {
+							date = df.monthBegin(array.getInt(i));
+						}
+						
+						result.pushInt(date);
+					}
+				}
+			} else if (isQuater) {
+				for (int i = 1; i <= size; ++i) {
+					if (array.isNull(i)) {
+						result.pushNull();
+					} else {
+						int date;
+						if (isEnd) {
+							date = df.quaterEnd(array.getInt(i));
+						} else {
+							date = df.quaterBegin(array.getInt(i));
+						}
+						
+						result.pushInt(date);
+					}
+				}
+			} else if (isYear) {
+				for (int i = 1; i <= size; ++i) {
+					if (array.isNull(i)) {
+						result.pushNull();
+					} else {
+						int date;
+						if (isEnd) {
+							date = df.yearEnd(array.getInt(i));
+						} else {
+							date = df.yearBegin(array.getInt(i));
+						}
+						
+						result.pushInt(date);
+					}
+				}
+			} else { // week
+				for (int i = 1; i <= size; ++i) {
+					if (array.isNull(i)) {
+						result.pushNull();
+					} else {
+						int date;
+						if (isEnd) {
+							if (isMondayFirst) {
+								date = df.weekEnd1(array.getInt(i));
+							} else {
+								date = df.weekEnd(array.getInt(i));
+							}
+						} else {
+							if (isMondayFirst) {
+								date = df.weekBegin1(array.getInt(i));
+							} else {
+								date = df.weekBegin(array.getInt(i));
+							}
+						}
+						
+						result.pushInt(date);
+					}
+				}
+			}
+			
+			return result;
+		} else {
+			ObjectArray result = new ObjectArray(size);
+			result.setTemporary(true);
+			
+			for (int i = 1; i <= size; ++i) {
+				Object date = pdate(array.get(i), option);
+				result.push(date);
+			}
+			
+			return result;
 		}
-
-		return DateFactory.get().weekBegin(date);
 	}
 }
