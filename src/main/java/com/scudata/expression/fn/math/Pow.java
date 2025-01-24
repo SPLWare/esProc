d4d6e365ad8fc6e15c3c531e1c162c537b5dc4c5
RQWangXiaoJun <wangxiaojun@raqsoft.com>
2025-01-24 11:31:49 +0800

    Do optimization.

--- src/main/java/com/scudata/expression/fn/math/Pow.java
+++ src/main/java/com/scudata/expression/fn/math/Pow.java
@@ -1,5 +1,12 @@
 package com.scudata.expression.fn.math;
 
+import com.scudata.array.ConstArray;
+import com.scudata.array.DoubleArray;
+import com.scudata.array.IArray;
+import com.scudata.array.IntArray;
+import com.scudata.array.LongArray;
+import com.scudata.array.NumberArray;
+import com.scudata.array.ObjectArray;
 import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
 import com.scudata.dm.Context;
@@ -55,4 +62,359 @@ public class Pow extends Function {
 								   Variant.doubleValue(result2)));
 	}
 
+	/**
+	 * 计算出所有行的结果
+	 * @param ctx 计算上行文
+	 * @return IArray
+	 */
+	public IArray calculateAll(Context ctx) {
+		if (param.isLeaf()) {
+			IArray array = param.getLeafExpression().calculateAll(ctx);
+			int size = array.size();
+			
+			if (array instanceof ConstArray) {
+				Object obj = array.get(1);
+				obj = Variant.square(obj);
+				return new ConstArray(obj, size);
+			} else if (array instanceof IntArray) {
+				IntArray numberArray = (IntArray)array;
+				IntArray result = new IntArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						int v = numberArray.getInt(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else if (array instanceof LongArray) {
+				LongArray numberArray = (LongArray)array;
+				LongArray result = new LongArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						long v = numberArray.getLong(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else if (array instanceof DoubleArray) {
+				DoubleArray numberArray = (DoubleArray)array;
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						double v = numberArray.getDouble(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else {
+				ObjectArray result = new ObjectArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					Object obj = array.get(i);
+					obj = Variant.square(obj);
+					result.push(obj);
+				}
+				
+				return result;
+			}
+		} else if (param.getSubSize() == 2) {
+			IParam sub0 = param.getSub(0);
+			IParam sub1 = param.getSub(1);
+			if (sub0 == null || sub1 == null) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("power" + mm.getMessage("function.invalidParam"));
+			}
+			
+			IArray array1 = sub0.getLeafExpression().calculateAll(ctx);
+			IArray array2 = sub1.getLeafExpression().calculateAll(ctx);
+			int size = array1.size();
+			
+			if (array2 instanceof ConstArray) {
+				Object obj2 = array2.get(1);
+				if (obj2 == null) {
+					return new ConstArray(null, size);
+				} else if (!(obj2 instanceof Number)) {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+				}
+				
+				double d2 = ((Number)obj2).doubleValue();
+				if (array1 instanceof ConstArray) {
+					Object obj1 = array1.get(1);
+					if (obj1 instanceof Number) {
+						double d1 = ((Number)obj1).doubleValue();
+						d1 = Math.pow(d1, d2);
+						return new ConstArray(new Double(d1), size);
+					} else if (obj1 == null) {
+						return new ConstArray(null, size);
+					} else {
+						MessageManager mm = EngineMessage.get();
+						throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+					}
+				}
+
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				if (array1 instanceof NumberArray) {
+					NumberArray numberArray = (NumberArray)array1;
+					for (int i = 1; i <= size; ++i) {
+						if (numberArray.isNull(i)) {
+							result.pushNull();
+						} else {
+							double v = Math.pow(numberArray.getDouble(i), d2);
+							result.push(v);
+						}
+					}
+				} else {
+					for (int i = 1; i <= size; ++i) {
+						Object obj1 = array1.get(i);
+						if (obj1 instanceof Number) {
+							double v = Math.pow(((Number)obj1).doubleValue(), d2);
+							result.push(v);
+						} else if (obj1 == null) {
+							result.pushNull();
+						} else {
+							MessageManager mm = EngineMessage.get();
+							throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+						}
+					}
+				}
+				
+				return result;
+			} else {
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					Object obj1 = array1.get(i);
+					Object obj2 = array2.get(i);
+					
+					if (obj1 == null || obj2 == null) {
+						result.pushNull();
+					} else if (obj1 instanceof Number && obj2 instanceof Number) {
+						double v = Math.pow(((Number)obj1).doubleValue(), ((Number)obj2).doubleValue());
+						result.push(v);
+					} else {
+						MessageManager mm = EngineMessage.get();
+						throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+					}
+				}
+				
+				return result;
+			}
+		} else {
+			MessageManager mm = EngineMessage.get();
+			throw new RQException("power" + mm.getMessage("function.invalidParam"));
+		}
+	}
+	
+	/**
+	 * 计算signArray中取值为sign的行
+	 * @param ctx
+	 * @param signArray 行标识数组
+	 * @param sign 标识
+	 * @return IArray
+	 */
+	public IArray calculateAll(Context ctx, IArray signArray, boolean sign) {
+		if (param.isLeaf()) {
+			IArray array = param.getLeafExpression().calculateAll(ctx, signArray, sign);
+			int size = array.size();
+			
+			if (array instanceof ConstArray) {
+				Object obj = array.get(1);
+				obj = Variant.square(obj);
+				return new ConstArray(obj, size);
+			}
+
+			boolean[] signDatas;
+			if (sign) {
+				signDatas = signArray.isTrue().getDatas();
+			} else {
+				signDatas = signArray.isFalse().getDatas();
+			}
+			
+			if (array instanceof IntArray) {
+				IntArray numberArray = (IntArray)array;
+				IntArray result = new IntArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (!signDatas[i] || numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						int v = numberArray.getInt(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else if (array instanceof LongArray) {
+				LongArray numberArray = (LongArray)array;
+				LongArray result = new LongArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (!signDatas[i] || numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						long v = numberArray.getLong(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else if (array instanceof DoubleArray) {
+				DoubleArray numberArray = (DoubleArray)array;
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (!signDatas[i] || numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						double v = numberArray.getDouble(i);
+						result.push(v * v);
+					}
+				}
+				
+				return result;
+			} else {
+				ObjectArray result = new ObjectArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (signDatas[i]) {
+						Object obj = array.get(i);
+						obj = Variant.square(obj);
+						result.push(obj);
+					} else {
+						result.pushNull();
+					}
+				}
+				
+				return result;
+			}
+		} else if (param.getSubSize() == 2) {
+			IParam sub0 = param.getSub(0);
+			IParam sub1 = param.getSub(1);
+			if (sub0 == null || sub1 == null) {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("power" + mm.getMessage("function.invalidParam"));
+			}
+			
+			IArray array1 = sub0.getLeafExpression().calculateAll(ctx);
+			IArray array2 = sub1.getLeafExpression().calculateAll(ctx);
+			int size = array1.size();
+			
+			boolean[] signDatas;
+			if (sign) {
+				signDatas = signArray.isTrue().getDatas();
+			} else {
+				signDatas = signArray.isFalse().getDatas();
+			}
+			
+			if (array2 instanceof ConstArray) {
+				Object obj2 = array2.get(1);
+				if (obj2 == null) {
+					return new ConstArray(null, size);
+				} else if (!(obj2 instanceof Number)) {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+				}
+				
+				double d2 = ((Number)obj2).doubleValue();
+				if (array1 instanceof ConstArray) {
+					Object obj1 = array1.get(1);
+					if (obj1 instanceof Number) {
+						double d1 = ((Number)obj1).doubleValue();
+						d1 = Math.pow(d1, d2);
+						return new ConstArray(new Double(d1), size);
+					} else if (obj1 == null) {
+						return new ConstArray(null, size);
+					} else {
+						MessageManager mm = EngineMessage.get();
+						throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+					}
+				}
+
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				if (array1 instanceof NumberArray) {
+					NumberArray numberArray = (NumberArray)array1;
+					for (int i = 1; i <= size; ++i) {
+						if (!signDatas[i] || numberArray.isNull(i)) {
+							result.pushNull();
+						} else {
+							double v = Math.pow(numberArray.getDouble(i), d2);
+							result.push(v);
+						}
+					}
+				} else {
+					for (int i = 1; i <= size; ++i) {
+						if (signDatas[i]) {
+							Object obj1 = array1.get(i);
+							if (obj1 instanceof Number) {
+								double v = Math.pow(((Number)obj1).doubleValue(), d2);
+								result.push(v);
+							} else if (obj1 == null) {
+								result.pushNull();
+							} else {
+								MessageManager mm = EngineMessage.get();
+								throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+							}
+						} else {
+							result.pushNull();
+						}
+					}
+				}
+				
+				return result;
+			} else {
+				DoubleArray result = new DoubleArray(size);
+				result.setTemporary(true);
+				
+				for (int i = 1; i <= size; ++i) {
+					if (signDatas[i]) {
+						Object obj1 = array1.get(i);
+						Object obj2 = array2.get(i);
+						
+						if (obj1 == null || obj2 == null) {
+							result.pushNull();
+						} else if (obj1 instanceof Number && obj2 instanceof Number) {
+							double v = Math.pow(((Number)obj1).doubleValue(), ((Number)obj2).doubleValue());
+							result.push(v);
+						} else {
+							MessageManager mm = EngineMessage.get();
+							throw new RQException("power" + mm.getMessage("function.paramTypeError"));
+						}
+					} else {
+						result.pushNull();
+					}
+				}
+				
+				return result;
+			}
+		} else {
+			MessageManager mm = EngineMessage.get();
+			throw new RQException("power" + mm.getMessage("function.invalidParam"));
+		}
+	}
 }
--- src/main/java/com/scudata/expression/fn/math/Sin.java
+++ src/main/java/com/scudata/expression/fn/math/Sin.java
@@ -1,5 +1,9 @@
 package com.scudata.expression.fn.math;
 
+import com.scudata.array.ConstArray;
+import com.scudata.array.DoubleArray;
+import com.scudata.array.IArray;
+import com.scudata.array.NumberArray;
 import com.scudata.common.MessageManager;
 import com.scudata.common.RQException;
 import com.scudata.dm.Context;
@@ -30,5 +34,128 @@ public class Sin extends Function {
 			MessageManager mm = EngineMessage.get();
 			throw new RQException("sin" + mm.getMessage("function.paramTypeError"));
 		}
+	}
+
+	/**
+	 * 计算出所有行的结果
+	 * @param ctx 计算上行文
+	 * @return IArray
+	 */
+	public IArray calculateAll(Context ctx) {
+		IArray array = param.getLeafExpression().calculateAll(ctx);
+		int size = array.size();
+		
+		if (array instanceof ConstArray) {
+			Object obj = array.get(1);
+			if (obj instanceof Number) {
+				double v = Math.sin(((Number)obj).doubleValue());
+				return new ConstArray(new Double(v), size);
+			} else if (obj == null) {
+				return new ConstArray(null, size);
+			} else {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sin" + mm.getMessage("function.paramTypeError"));
+			}
+		}
+		
+		DoubleArray result = new DoubleArray(size);
+		result.setTemporary(true);
+		
+		if (array instanceof NumberArray) {
+			NumberArray numberArray = (NumberArray)array;
+			for (int i = 1; i <= size; ++i) {
+				if (numberArray.isNull(i)) {
+					result.pushNull();
+				} else {
+					double v = Math.sin(numberArray.getDouble(i));
+					result.push(v);
+				}
+			}
+		} else {
+			for (int i = 1; i <= size; ++i) {
+				Object obj = array.get(i);
+				if (obj instanceof Number) {
+					double v = Math.sin(((Number)obj).doubleValue());
+					result.push(v);
+				} else if (obj == null) {
+					result.pushNull();
+				} else {
+					MessageManager mm = EngineMessage.get();
+					throw new RQException("sin" + mm.getMessage("function.paramTypeError"));
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 计算signArray中取值为sign的行
+	 * @param ctx
+	 * @param signArray 行标识数组
+	 * @param sign 标识
+	 * @return IArray
+	 */
+	public IArray calculateAll(Context ctx, IArray signArray, boolean sign) {
+		IArray array = param.getLeafExpression().calculateAll(ctx, signArray, sign);
+		int size = array.size();
+		
+		if (array instanceof ConstArray) {
+			Object obj = array.get(1);
+			if (obj instanceof Number) {
+				double v = Math.sin(((Number)obj).doubleValue());
+				return new ConstArray(new Double(v), size);
+			} else if (obj == null) {
+				return new ConstArray(null, size);
+			} else {
+				MessageManager mm = EngineMessage.get();
+				throw new RQException("sin" + mm.getMessage("function.paramTypeError"));
+			}
+		}
+
+		boolean[] signDatas;
+		if (sign) {
+			signDatas = signArray.isTrue().getDatas();
+		} else {
+			signDatas = signArray.isFalse().getDatas();
+		}
+		
+		DoubleArray result = new DoubleArray(size);
+		result.setTemporary(true);
+		
+		if (array instanceof NumberArray) {
+			NumberArray numberArray = (NumberArray)array;
+			for (int i = 1; i <= size; ++i) {
+				if (signDatas[i]) {
+					if (numberArray.isNull(i)) {
+						result.pushNull();
+					} else {
+						double v = Math.sin(numberArray.getDouble(i));
+						result.push(v);
+					}
+				} else {
+					result.pushNull();
+				}
+			}
+		} else {
+			for (int i = 1; i <= size; ++i) {
+				if (signDatas[i]) {
+					Object obj = array.get(i);
+					if (obj instanceof Number) {
+						double v = Math.sin(((Number)obj).doubleValue());
+						result.push(v);
+					} else if (obj == null) {
+						result.pushNull();
+					} else {
+						MessageManager mm = EngineMessage.get();
+						throw new RQException("sin" + mm.getMessage("function.paramTypeError"));
+					}
+				} else {
+					result.pushNull();
+				}
+			}
+		}
+		
+		return result;
 	}
 }
