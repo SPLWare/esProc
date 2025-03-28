e28b5c970c8d8e43771139cd00e73ddd73cab1cc
minimaomi <294973991@qq.com>
2025-03-28 18:25:11 +0800

    delete unit

--- bin/ServerConsole.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "setEnv.bat"
-start "dm" %EXECJAVA% -Xms128m -Xmx9444m  -cp %START_HOME%\esProc\classes;%RAQCLASSPATH% -Duser.language=%language% -Dstart.home=%START_HOME%\esProc com.scudata.ide.spl.ServerConsole %1 %2 
--- bin/ServerConsole.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-source setEnv.sh
-"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$RAQCLASSPATH" -Duser.language="$language" -Dstart.home="$START_HOME"/esProc com.scudata.ide.spl.ServerConsole $1 $2 
