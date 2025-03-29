ab93cf8a76fd8e43e3b5672a10dec55d5f66ba95
minimaomi <294973991@qq.com>
2025-03-29 11:43:49 +0800

    update bin

--- bin/setEnv.bat
+++ bin/setEnv.bat
@@ -5,17 +5,8 @@ set EXECJAVAW=%JAVA_HOME%\jre\bin\javaw
 set RAQCLASSPATH=%START_HOME%\esProc\lib\*;%START_HOME%\common\jdbc\*
 set language=en
 set "configFile=%START_HOME%\esProc\bin\config.txt"
-set "targetKey=jvm_args"
-for /f "usebackq delims=" %%i in (`powershell -Command "Get-Content '%configFile%' | Where-Object { $_ -match '^%targetKey%=.*' } | ForEach-Object { $_.Trim() -replace '^%targetKey%=|;$' }"`) do (
-    set "value=%%i"
-    goto :breakLoop
+setlocal enabledelayedexpansion
+for /f "usebackq delims=" %%a in (`powershell -Command "$content = Get-Content -Path 'configFile'; ($content -split '=')[-1] -split ';' | Select-Object -First 1"`) do (
+    set "jvm_args=%%a"
 )
-:breakLoop
-if defined value (
-    set "value = !valueWithKey:%targetKey% =!"
-) else (
-	echo ""
-)
-set v1=%value% 
-set "jvm_args=%v1:~9%"
-echo %jvm_args%
+endlocal
