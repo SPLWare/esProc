743b5d506b3909d600b44ceb39303e6157bf6b8c
minimaomi <294973991@qq.com>
2025-04-15 17:44:54 +0800

    update esprocx

--- bin/esprocx.sh
+++ bin/esprocx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source setEnv.sh
-"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$RAQCLASSPATH"  -Duser.language="$language" -Dstart.home="$START_HOME"/esProc com.scudata.ide.spl.Esprocx $@
+"$EXEC_JAVA" -Xms128m -Xmx1024m  -cp "$START_HOME"/esProc/classes:"$RAQCLASSPATH"  -Duser.language="$language" -Dstart.home="$START_HOME"/esProc com.scudata.ide.spl.Esprocx "$@"
