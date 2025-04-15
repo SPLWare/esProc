ce10b0d143beca4ea0f9f6f2bc59cc8b3e90aced
minimaomi <294973991@qq.com>
2025-04-15 17:04:43 +0800

    update esprocx

--- bin/esprocx.bat
+++ bin/esprocx.bat
@@ -1,3 +1,3 @@
 @echo off
 call "setEnv.bat"
-%EXECJAVA% -Xms128m -Xmx4818m  -cp %START_HOME%\esProc\classes;%RAQCLASSPATH% -Duser.language=%language% -Dstart.home=%START_HOME%\esProc   com.scudata.ide.spl.Esprocx  %1 %2 
+%EXECJAVA% -Xms128m -Xmx4818m  -cp %START_HOME%\esProc\classes;%RAQCLASSPATH% -Duser.language=%language% -Dstart.home=%START_HOME%\esProc   com.scudata.ide.spl.Esprocx  %*
